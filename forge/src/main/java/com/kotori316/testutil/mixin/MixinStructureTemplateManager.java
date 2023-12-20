806e22c32b9f208711bd8e345bfee1477c23464b
Kotori316 <Kotori316@gmail.com>
2023-12-21 00:12:59 +0900

    Add opcode arg

--- forge/src/main/java/com/kotori316/testutil/mixin/MixinStructureTemplateManager.java
+++ forge/src/main/java/com/kotori316/testutil/mixin/MixinStructureTemplateManager.java
@@ -1,13 +1,14 @@
 package com.kotori316.testutil.mixin;
 
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(StructureTemplateManager.class)
 public abstract class MixinStructureTemplateManager {
-    @Redirect(method = "<init>", at = @At(value = "FIELD", target = "Lnet/minecraft/SharedConstants;IS_RUNNING_IN_IDE:Z"))
+    @Redirect(method = "<init>", at = @At(value = "FIELD", target = "Lnet/minecraft/SharedConstants;IS_RUNNING_IN_IDE:Z", opcode = Opcodes.GETSTATIC))
     private boolean injectTestStructures() {
         return true;
     }
