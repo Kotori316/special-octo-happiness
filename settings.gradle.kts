dd8a808b50d0933eb0e91b0999a7d14074d7e975
Kotori316 <Kotori316@gmail.com>
2023-12-30 08:26:55 +0900

    Enabled build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,16 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.7.0")
+    id("com.gradle.enterprise") version ("3.+")
+}
+
+gradleEnterprise {
+    buildScan {
+        if (System.getenv("CI").toBoolean()) {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
 }
 
 includeBuild("build-logic")
