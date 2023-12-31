c182950717f2448de7c79b20f44b6854374e44cc
Kotori316 <Kotori316@gmail.com>
2023-12-31 15:29:17 +0900

    Find current log reporter

--- test-utility/common/src/main/java/com/kotori316/testutil/common/reporter/ReporterRegister.java
+++ test-utility/common/src/main/java/com/kotori316/testutil/common/reporter/ReporterRegister.java
@@ -1,16 +1,18 @@
 package com.kotori316.testutil.common.reporter;
 
 import net.minecraft.gametest.framework.GlobalTestReporter;
-import net.minecraft.gametest.framework.LogTestReporter;
+import net.minecraft.gametest.framework.TestReporter;
 
+import java.lang.reflect.Modifier;
 import java.nio.file.Path;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
+import java.util.stream.Stream;
 
 public class ReporterRegister {
-    static final DateTimeFormatter FORMATTER =  new DateTimeFormatterBuilder()
+    static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
         .append(DateTimeFormatter.ISO_LOCAL_DATE)
         .appendLiteral('T')
         .appendValue(ChronoField.HOUR_OF_DAY, 2)
@@ -21,7 +23,21 @@ public class ReporterRegister {
     public static void changeReporter() {
         var time = ZonedDateTime.now().format(FORMATTER);
         GlobalTestReporter.replaceWith(
-            new CombinedReporter(new LogTestReporter(), new CreateFileReporter(Path.of("crash-reports/" + time)))
+            new CombinedReporter(current(), new CreateFileReporter(Path.of("crash-reports/" + time)))
         );
+    }
+
+    private static TestReporter current() {
+        try {
+            var field = Stream.of(GlobalTestReporter.class.getDeclaredFields())
+                .filter(f -> f.getType() == TestReporter.class)
+                .filter(f -> (f.getModifiers() & Modifier.STATIC) == Modifier.STATIC)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No TestReporter field found in GlobalTestReporter"));
+            field.setAccessible(true);
+            return (TestReporter) field.get(null);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
