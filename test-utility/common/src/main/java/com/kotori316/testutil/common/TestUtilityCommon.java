e11eee13c446efd601c4079083f68282e339b27b
Kotori316 <Kotori316@gmail.com>
2023-12-31 19:41:26 +0900

    Set env value

--- test-utility/common/src/main/java/com/kotori316/testutil/common/TestUtilityCommon.java
+++ test-utility/common/src/main/java/com/kotori316/testutil/common/TestUtilityCommon.java
@@ -2,6 +2,7 @@ package com.kotori316.testutil.common;
 
 import com.google.common.hash.HashCode;
 import net.minecraft.gametest.framework.TestFunction;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,14 +15,18 @@ public class TestUtilityCommon {
     public static final Logger DATA_GENERATOR_LOGGER = LoggerFactory.getLogger("TestUtil/DataGen");
     public static final Logger TEST_LOADER_LOGGER = LoggerFactory.getLogger("TestUtil/TestLoad");
 
-    public static void logTestName(Object maybeTest, Method createFrom) {
-        if (maybeTest instanceof TestFunction testFunction) {
-            TEST_LOADER_LOGGER.info("Register {}(batch: {}, structure: {}) from {}",
-                testFunction.getTestName(), testFunction.getBatchName(), testFunction.getStructureName(), createFrom);
+    public static void logTestName(Object maybeTest, @Nullable Method createFrom) {
+        if (Boolean.parseBoolean(System.getenv("TEST_UTILITY_LOG_ALL_TEST"))) {
+            if (maybeTest instanceof TestFunction testFunction) {
+                TEST_LOADER_LOGGER.info("Register {}(batch: {}, structure: {}) from {}",
+                    testFunction.getTestName(), testFunction.getBatchName(), testFunction.getStructureName(), createFrom);
+            }
         }
     }
 
-    public static void  logDataGeneration(Path path, HashCode hashCode) {
-        DATA_GENERATOR_LOGGER.info("Generating {}", path.toAbsolutePath().normalize());
+    public static void logDataGeneration(Path path, HashCode hashCode) {
+        if (Boolean.parseBoolean(System.getenv("TEST_UTILITY_LOG_ALL_DATA"))) {
+            DATA_GENERATOR_LOGGER.info("Generating {} at {}", hashCode.toString(), path.toAbsolutePath().normalize());
+        }
     }
 }
