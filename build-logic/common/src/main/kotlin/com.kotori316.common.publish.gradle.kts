fd22cbc36e2abe13db12e87fe70d5a74d729db9b
Kotori316 <Kotori316@gmail.com>
2024-01-14 14:30:08 +0900

    Set platform version

---
+++ build-logic/common/src/main/kotlin/com.kotori316.common.publish.gradle.kts
@@ -0,0 +1,81 @@
+import com.kotori316.plugin.cf.CallVersionCheckFunctionTask
+import com.kotori316.plugin.cf.CallVersionFunctionTask
+
+plugins {
+    id("maven-publish")
+    id("com.kotori316.plugin.cf")
+}
+
+val mc: String = project.property("minecraft").toString()
+val releaseDebug: Boolean = (System.getenv("RELEASE_DEBUG") ?: "true").toBoolean()
+
+fun artifactName(): String {
+    val p = project.path
+    return p.split(':')[1]
+}
+
+fun pfVersion(platform: String): String {
+    return when (platform) {
+        "forge" -> project.property("forge_version").toString()
+        "fabric" -> project.property("fabric_version").toString()
+        "neoforge" -> project.property("neo_version").toString()
+        else -> throw IllegalArgumentException("Unknown platform: $platform")
+    }
+}
+
+// configure the maven publication
+publishing {
+    publications {
+        if (!releaseDebug) {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+                artifactId = "${artifactName()}-${project.name}"
+                pom {
+                    description = project.provider { project.ext.get("generalDescription") as String }
+                }
+            }
+        }
+        create<MavenPublication>("mavenLatest") {
+            from(components["java"])
+            artifactId = "${artifactName()}-${project.name}"
+            version = project.property("maven_latest").toString()
+            pom {
+                description = project.provider { project.ext.get("generalDescription") as String }
+            }
+        }
+    }
+
+    repositories {
+        if (System.getenv("CLOUDFLARE_S3_ENDPOINT") != null) {
+            val r2AccessKey = (project.findProperty("r2_access_key") ?: System.getenv("R2_ACCESS_KEY") ?: "") as String
+            val r2SecretKey = (project.findProperty("r2_secret_key") ?: System.getenv("R2_SECRET_KEY") ?: "") as String
+            maven {
+                name = "kotori316-maven"
+                url = uri("s3://kotori316-maven")
+                credentials(AwsCredentials::class) {
+                    accessKey = r2AccessKey
+                    secretKey = r2SecretKey
+                }
+            }
+        }
+    }
+}
+
+tasks.register("registerVersion", CallVersionFunctionTask::class) {
+    functionEndpoint = CallVersionFunctionTask.readVersionFunctionEndpoint(project)
+    gameVersion = mc
+    platform = project.name
+    platformVersion = pfVersion(project.name)
+    modName = artifactName()
+    changelog = project.provider { project.ext.get("generalDescription") as String }
+    homepage = "https://github.com/Kotori316/special-octo-happiness"
+    isDryRun = releaseDebug
+}
+
+tasks.register("checkReleaseVersion", CallVersionCheckFunctionTask::class) {
+    gameVersion = mc
+    platform = project.name
+    modName = artifactName()
+    version = project.version.toString()
+    failIfExists = !releaseDebug
+}
--- build-logic/common/src/main/kotlin/com.kotori316.tu.platforms.gradle.kts
+++ build-logic/common/src/main/kotlin/com.kotori316.tu.platforms.gradle.kts
@@ -1,69 +1,11 @@
-import com.kotori316.plugin.cf.CallVersionCheckFunctionTask
-import com.kotori316.plugin.cf.CallVersionFunctionTask
-
 plugins {
     id("java")
-    id("maven-publish")
-    id("com.kotori316.plugin.cf")
     id("com.kotori316.common.signing")
+    id("com.kotori316.common.publish")
 }
 
 val mc: String = project.property("minecraft").toString()
 val releaseDebug: Boolean = (System.getenv("RELEASE_DEBUG") ?: "true").toBoolean()
 
 val generalDescription = "Test Utility(${project.version}) for Minecraft $mc with ${project.name}"
-
-// configure the maven publication
-publishing {
-    publications {
-        if (!releaseDebug) {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
-                artifactId = "test-utility-${project.name}"
-                pom {
-                    description = generalDescription
-                }
-            }
-        }
-        create<MavenPublication>("mavenLatest") {
-            from(components["java"])
-            artifactId = "test-utility-${project.name}"
-            version = project.property("maven_latest").toString()
-            pom {
-                description = generalDescription
-            }
-        }
-    }
-
-    repositories {
-        if (System.getenv("CLOUDFLARE_S3_ENDPOINT") != null) {
-            val r2AccessKey = (project.findProperty("r2_access_key") ?: System.getenv("R2_ACCESS_KEY") ?: "") as String
-            val r2SecretKey = (project.findProperty("r2_secret_key") ?: System.getenv("R2_SECRET_KEY") ?: "") as String
-            maven {
-                name = "kotori316-maven"
-                url = uri("s3://kotori316-maven")
-                credentials(AwsCredentials::class) {
-                    accessKey = r2AccessKey
-                    secretKey = r2SecretKey
-                }
-            }
-        }
-    }
-}
-
-tasks.register("registerVersion", CallVersionFunctionTask::class) {
-    functionEndpoint = CallVersionFunctionTask.readVersionFunctionEndpoint(project)
-    gameVersion = mc
-    platform = project.name
-    modName = "test-utility"
-    changelog = generalDescription
-    homepage = "https://github.com/Kotori316/special-octo-happiness"
-}
-
-tasks.register("checkReleaseVersion", CallVersionCheckFunctionTask::class) {
-    gameVersion = mc
-    platform = project.name
-    modName = "test-utility"
-    version = project.version.toString()
-    failIfExists = !releaseDebug
-}
+ext.set("generalDescription", generalDescription)
