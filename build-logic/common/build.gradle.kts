4a5a36a823759b4fb323e9169cf6be67f7f2e1a7
Kotori316 <Kotori316@gmail.com>
2024-05-11 00:02:04 +0900

    Revert cf repo

--- build-logic/common/build.gradle.kts
+++ build-logic/common/build.gradle.kts
@@ -9,10 +9,9 @@ repositories {
 }
 
 dependencies {
-    /*mapOf(
+    mapOf(
         "com.kotori316.plugin.cf" to libs.versions.gradle.cf.get(),
     ).forEach { (name, version) ->
         implementation(group = name, name = "${name}.gradle.plugin", version = version)
-    }*/
-    implementation(group = "com.kotori316", name = "call-plugin", version = libs.versions.gradle.cf.get())
+    }
 }
