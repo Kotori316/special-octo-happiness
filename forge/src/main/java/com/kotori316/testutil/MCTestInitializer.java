fa38c26283f87f5b0c9446f6ec48b40e7a6cfa4a
Kotori316 <Kotori316@gmail.com>
2025-02-12 09:20:35 +0900

    Create dummy mod file info for forge

--- forge/src/main/java/com/kotori316/testutil/MCTestInitializer.java
+++ forge/src/main/java/com/kotori316/testutil/MCTestInitializer.java
@@ -30,6 +30,7 @@ import net.minecraftforge.forgespi.language.IConfigurable;
 import net.minecraftforge.forgespi.language.IModFileInfo;
 import net.minecraftforge.forgespi.language.IModInfo;
 import net.minecraftforge.forgespi.locating.ForgeFeature;
+import net.minecraftforge.forgespi.locating.IModFile;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.registries.*;
 import net.minecraftforge.server.LanguageHook;
@@ -351,9 +352,9 @@ public final class MCTestInitializer implements BeforeAllCallback {
         }
     }
 
-    @SuppressWarnings("ClassCanBeRecord")
     private static final class DummyModInfo implements IModInfo, IConfigurable {
         private final String name;
+        private final DummyModFileInfo modFileInfo = new DummyModFileInfo(this);
 
         private DummyModInfo(String name) {
             this.name = name;
@@ -361,7 +362,7 @@ public final class MCTestInitializer implements BeforeAllCallback {
 
         @Override
         public IModFileInfo getOwningFile() {
-            return null;
+            return modFileInfo;
         }
 
         @Override
@@ -447,6 +448,65 @@ public final class MCTestInitializer implements BeforeAllCallback {
         public String toString() {
             return "DummyModInfo[" +
                 "name=" + name + ']';
+        }
+    }
+
+    @SuppressWarnings("ClassCanBeRecord")
+    private static final class DummyModFileInfo implements IModFileInfo {
+        private final DummyModInfo modInfo;
+
+        private DummyModFileInfo(DummyModInfo modInfo) {
+            this.modInfo = modInfo;
+        }
+
+        @Override
+        public List<IModInfo> getMods() {
+            return List.of(modInfo);
+        }
+
+        @Override
+        public List<LanguageSpec> requiredLanguageLoaders() {
+            return List.of();
+        }
+
+        @Override
+        public boolean showAsResourcePack() {
+            return false;
+        }
+
+        @Override
+        public Map<String, Object> getFileProperties() {
+            return Map.of();
+        }
+
+        @Override
+        public String getLicense() {
+            return "";
+        }
+
+        @Override
+        public String moduleName() {
+            return "";
+        }
+
+        @Override
+        public String versionString() {
+            return "";
+        }
+
+        @Override
+        public List<String> usesServices() {
+            return List.of();
+        }
+
+        @Override
+        public IModFile getFile() {
+            return null;
+        }
+
+        @Override
+        public IConfigurable getConfig() {
+            return modInfo;
         }
     }
 }
