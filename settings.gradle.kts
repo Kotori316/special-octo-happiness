2ff56cae19f8d030290f315f6a580a843e62e372
Kotori316 <Kotori316@gmail.com>
2025-04-13 22:45:42 +0900

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,8 +12,8 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
-    id("com.gradle.develocity") version ("3.+")
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.10.0")
+    id("com.gradle.develocity") version ("4.+")
 }
 
 develocity {
