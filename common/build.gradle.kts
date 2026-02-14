57f4a42c0425937d425d0032a21fb14480e0919c
Kotori316 <Kotori316@gmail.com>
2026-02-14 23:17:02 +0900

    Vanilla gradle config

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.kotori316.common.java")
-    id("org.spongepowered.gradle.vanilla") version ("0.2.2")
+    alias(libs.plugins.spongepowered.vanilla)
 }
 
 val mc: String = project.property("minecraft").toString()
