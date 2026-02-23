f372789e5219ff0ee98ec516ac47169d647cf4fa
Kotori316 <Kotori316@gmail.com>
2026-02-23 23:53:19 +0900

    Fix annotation in Fabric

--- fabric/src/main/java/com/kotori316/testutil/mixin/FabricGameTestHelperMixin.java
+++ fabric/src/main/java/com/kotori316/testutil/mixin/FabricGameTestHelperMixin.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@SuppressWarnings("UnstableApiUsage")
 @Mixin(FabricGameTestRunner.class)
 public class FabricGameTestHelperMixin {
     @SuppressWarnings("DiscouragedShift")
