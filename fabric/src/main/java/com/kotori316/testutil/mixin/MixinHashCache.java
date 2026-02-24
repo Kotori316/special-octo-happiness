d216acbeb945f5c9bd38fb8e8e9cccb6288fd0c0
Kotori316 <Kotori316@gmail.com>
2026-02-24 09:34:27 +0900

    Add mixin for other platform

---
+++ fabric/src/main/java/com/kotori316/testutil/mixin/MixinHashCache.java
@@ -0,0 +1,26 @@
+package com.kotori316.testutil.mixin;
+
+import com.kotori316.testutil.common.HashCacheIgnore;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.data.HashCache;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.nio.file.Path;
+import java.util.Set;
+
+@Mixin(HashCache.class)
+public final class MixinHashCache {
+    @Final
+    @Shadow
+    private Path rootDir;
+
+    @Inject(method = "purgeStaleAndWrite", at = @At(value = "INVOKE", target = "Ljava/util/Set;add(Ljava/lang/Object;)Z", shift = At.Shift.AFTER))
+    private void addMetaInfToIgnoreSet(CallbackInfo ci, @Local(ordinal = 0) Set<Path> set) {
+        HashCacheIgnore.addMetaInfToIgnoreSet(rootDir, set);
+    }
+}
